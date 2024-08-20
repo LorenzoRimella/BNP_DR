ec98014dcc8828c9a87d83b15e7a7bbee794bf4e
LorenzoRimella <lorenzo.rimella@hotmail.it>
2024-08-20 17:08:30 +0100

    fix tutorial

--- experiments/SZ_NY/SZ_BNP_mc/SZ_NY_tau.py
+++ experiments/SZ_NY/SZ_BNP_mc/SZ_NY_tau.py
@@ -8,15 +8,16 @@ import tensorflow_probability as tfp
 import time
 
 import sys
-sys.path.append('scripts/')
+sys.path.append('DisclosureRisk/Scripts/')
 from BNP import *
 from BNP_structural_zeros import *
 
 task_id =  int(os.getenv("SLURM_ARRAY_TASK_ID"))-1
 
+seed_to_use = 42
 n = [1000, 5000, 10000][task_id]
-input_path =  "data/structural_zeros/"
-output_path = "data/structural_zeros/SZ_BNP/mc/"+str(n)+"/"
+input_path =  "DisclosureRisk/Data/NY/structural_zeros/"
+output_path = "DisclosureRisk/Data/NY/structural_zeros/SZ_BNP_sample_"+str(seed_to_use)+"/mc/"+str(n)+"/"
 if not os.path.exists(output_path):
 
     os.makedirs(output_path)
@@ -35,8 +36,6 @@ if gpus:
     # Enable memory growth for each GPU
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
-
-seed_to_use = [1042, 1234, 123][task_id]
 
 tf.random.set_seed((seed_to_use+n))
 np.random.seed((seed_to_use+n))
@@ -60,13 +59,13 @@ disjoint_constraints = disjoint_constraint -1
 batch_size = 100
 N = 953076
 
-MCMC_iterations = 10
+MCMC_iterations = 100
 
 seed_MCMC_start, seed_MCMC_after_start  = tfp.random.split_seed( seed_to_use, n=2, salt='seed_MCMC_start_'+str(n))
 
 output = SZ_BNP_MCMC_from_start(X_ij, disjoint_constraints, one_n_j, K_initial, K_max, prior_MCMC, MCMC_iterations, seed_MCMC_start, "multiple", "fixed a,b")
 
-string = ["Start MCMC with n "+str(n), "\n"]
+string = ["Start MCMC with seed "+str(seed_to_use)+" and n "+str(n), "\n"]
 f= open(output_path+"Check/"+name_sim+".txt", "a")
 f.writelines(string)
 f.close()
@@ -75,7 +74,7 @@ initialization_MCMC = tuple(elem[-1,...] for elem in output)
 
 seed_MCMC_before_tau, seed_MCMC_after_tau  = tfp.random.split_seed( seed_MCMC_after_start, n=2, salt='seed_MCMC_before_after_tau_'+str(n))
 
-before_tau_iterations = 99
+before_tau_iterations = 990
 seed_MCMC_before_tau_splitted  = tfp.random.split_seed( seed_MCMC_before_tau, n=before_tau_iterations, salt='seed_MCMC_before_tau_'+str(n))
 for i in range(before_tau_iterations):
 
@@ -87,6 +86,13 @@ for i in range(before_tau_iterations):
     output = SZ_BNP_MCMC_initialized(X_ij, disjoint_constraints, one_n_j, K_max, prior_MCMC, initialization_MCMC, MCMC_iterations, seed_MCMC_before_tau_splitted[i], "multiple", "fixed a,b")
     initialization_MCMC = tuple(elem[-1,...] for elem in output)
 
+#     key_list = ["a", "b", "alpha_0", "alpha_i", "theta_k_full", "beta_k_00_full", "pi_i_0k_full", "K_current"]
+#     current_MCMC = {}
+#     for i in range(len(key_list)):
+#         current_MCMC[key_list[i]] = initialization_MCMC[i]
+        
+#     np.save(output_path+name_sim+"MCMC_initialization.npy", current_MCMC)
+
 K_list = []
 alpha_0_list = []
 alpha_i_list = []
@@ -94,7 +100,7 @@ tau_list = []
 a_list = []
 b_list = []
 
-after_tau_iterations = 10000
+after_tau_iterations = 1000
 seed_MCMC_after_tau_splitted  = tfp.random.split_seed( seed_MCMC_after_tau, n=after_tau_iterations, salt='seed_MCMC_after_tau_'+str(n))
 for i in range(after_tau_iterations):
     
